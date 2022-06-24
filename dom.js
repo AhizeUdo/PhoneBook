6767bb4edb637b2dcae1eb3c9a50f5a3ce6928fb
Vladhex12 <infohlfdz@gmail.com>
2022-06-24 13:50:49 +0100

    Changes made to UI

--- dom.js
+++ dom.js
@@ -1,9 +1,12 @@
 var form = document.getElementById('addForm');
-
 var itemList = document.getElementById('items');
-
+var filter = document.getElementById('filter')
 form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem)
+//to check for keys as you input immediately 
+filter.addEventListener('keyup', filterItems)
 
+//Add item
 function addItem(e){
     e.preventDefault();
     // console.log('working')
@@ -17,4 +20,28 @@ function addItem(e){
     deleteBtn.appendChild(document.createTextNode('X'));
     li.appendChild(deleteBtn);
     itemList.appendChild(li);
+}
+
+//Remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
+
+//Filter item 
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+    // console.log(text);
+    var items = itemList.getElementsByTagName('li')
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        // console.log(itemName);
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    }); 
 }
